44215cc150ce244919f68590bab6c59a9ae391f8
kanaru0928 <kanaru0928@gmail.com>
2024-08-21 18:49:43 +0900

    feat: Added binary controller

--- src/rs_binary/numbers.rs
+++ src/rs_binary/numbers.rs
@@ -1,4 +1,4 @@
-use super::base::{Decodable, Encodable, Codable, PointeredBinary};
+use super::base::{Codable, Decodable, Encodable, PointeredBinary};
 
 macro_rules! impl_number_encodable {
     ($type:ty) => {
@@ -28,7 +28,7 @@ macro_rules! impl_number_codable {
     ($type:ty) => {
         impl_number_encodable!($type);
         impl_number_decodable!($type);
-        
+
         impl Codable for $type {}
     };
 }
@@ -42,7 +42,7 @@ impl_number_codable!(f64);
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn test_u8_encodable() {
         let number: u8 = 0x12;
@@ -50,7 +50,7 @@ mod tests {
         let data = encoded.get_data();
         assert_eq!(data, &[0x12]);
     }
-    
+
     #[test]
     fn test_u8_decodable() {
         let data = vec![0x12];
@@ -74,7 +74,7 @@ mod tests {
         let number = u16::decode(&mut binary);
         assert_eq!(number, 0x1234);
     }
-    
+
     #[test]
     fn test_i32_encodable() {
         let number: i32 = 0x12345678;
@@ -82,7 +82,7 @@ mod tests {
         let data = encoded.get_data();
         assert_eq!(data, &[0x78, 0x56, 0x34, 0x12]);
     }
-    
+
     #[test]
     fn test_i32_decodable() {
         let data = vec![0x78, 0x56, 0x34, 0x12];
@@ -90,7 +90,7 @@ mod tests {
         let number = i32::decode(&mut binary);
         assert_eq!(number, 0x12345678);
     }
-    
+
     #[test]
     fn test_f32_encodable() {
         let number: f32 = 3.14;
@@ -98,7 +98,7 @@ mod tests {
         let data = encoded.get_data();
         assert_eq!(data, &[0xc3, 0xf5, 0x48, 0x40]);
     }
-    
+
     #[test]
     fn test_f32_decodable() {
         let data = vec![0xc3, 0xf5, 0x48, 0x40];
@@ -106,7 +106,7 @@ mod tests {
         let number = f32::decode(&mut binary);
         assert_eq!(number, 3.14);
     }
-    
+
     #[test]
     fn test_f64_encodable() {
         let number: f64 = 3.14;
@@ -114,7 +114,7 @@ mod tests {
         let data = encoded.get_data();
         assert_eq!(data, &[0x1f, 0x85, 0xeb, 0x51, 0xb8, 0x1e, 0x09, 0x40]);
     }
-    
+
     #[test]
     fn test_f64_decodable() {
         let data = vec![0x1f, 0x85, 0xeb, 0x51, 0xb8, 0x1e, 0x09, 0x40];
--- src/rs_binary/string.rs
+++ src/rs_binary/string.rs
@@ -1,5 +1,5 @@
+use super::base::{BinaryController, Codable, Decodable, Encodable, PointeredBinary};
 use num_traits::{FromPrimitive, NumCast};
-use super::base::{Codable, Decodable, Encodable, PointeredBinary};
 
 struct DynamicString<T: Codable + NumCast + FromPrimitive> {
     data: String,
@@ -41,6 +41,68 @@ impl<T: Codable + NumCast + FromPrimitive> Decodable for DynamicString<T> {
     }
 }
 
+impl<T: Codable + NumCast + FromPrimitive> Codable for DynamicString<T> {}
+
+pub struct DynamicStringBinaryController<T: Codable + NumCast + FromPrimitive> {
+    _marker: std::marker::PhantomData<T>,
+}
+
+impl<T: Codable + NumCast + FromPrimitive> DynamicStringBinaryController<T> {
+    pub fn new() -> Self {
+        DynamicStringBinaryController {
+            _marker: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<T: Codable + NumCast + FromPrimitive> BinaryController<String>
+    for DynamicStringBinaryController<T>
+{
+    fn encode(&self, data: String) -> PointeredBinary {
+        let dynamic_string: DynamicString<T> = data.into();
+        dynamic_string.encode()
+    }
+
+    fn decode(&self, data: &mut PointeredBinary) -> String {
+        let dynamic_string = DynamicString::<T>::decode(data);
+        dynamic_string.data
+    }
+}
+
+pub struct SizedStringBinaryController {
+    length: usize,
+}
+
+impl SizedStringBinaryController {
+    pub fn new(length: usize) -> Self {
+        SizedStringBinaryController { length }
+    }
+}
+
+impl BinaryController<String> for SizedStringBinaryController {
+    fn encode(&self, data: String) -> PointeredBinary {
+        let mut binary = PointeredBinary::new(Vec::new());
+        let mut bytes: Vec<u8> = data.as_bytes().to_vec();
+        bytes.resize(self.length, 0);
+        binary.write(bytes);
+        binary
+    }
+
+    fn decode(&self, data: &mut PointeredBinary) -> String {
+        let bytes = data.read(self.length).unwrap();
+        let mut string = String::new();
+        let mut length = 0;
+        for byte in bytes.clone().into_iter() {
+            if byte == 0 {
+                break;
+            }
+            length += 1;
+        }
+        string.push_str(std::str::from_utf8(&bytes[0..length]).unwrap());
+        string
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -51,31 +113,98 @@ mod tests {
         let dynamic_string: DynamicString<u8> = string.into();
         let encoded = dynamic_string.encode();
         let data = encoded.get_data();
-        assert_eq!(data, &[13, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33]);
+        assert_eq!(
+            data,
+            &[13, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33]
+        );
     }
 
     #[test]
     fn test_dynamic_string_decodable() {
-        let data = vec![13, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33];
+        let data = vec![
+            13, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33,
+        ];
         let mut binary = PointeredBinary::new(data);
         let dynamic_string = DynamicString::<u8>::decode(&mut binary);
         assert_eq!(dynamic_string.data, "Hello, World!");
     }
-    
+
     #[test]
     fn test_dynamic_u16_string_encodable() {
         let string = "Hello, World!".to_string();
         let dynamic_string: DynamicString<u16> = string.into();
         let encoded = dynamic_string.encode();
         let data = encoded.get_data();
-        assert_eq!(data, &[13, 0, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33]);
+        assert_eq!(
+            data,
+            &[13, 0, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33]
+        );
     }
-    
+
     #[test]
     fn test_dynamic_u16_string_decodable() {
-        let data = vec![13, 0, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33];
+        let data = vec![
+            13, 0, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33,
+        ];
         let mut binary = PointeredBinary::new(data);
         let dynamic_string = DynamicString::<u16>::decode(&mut binary);
         assert_eq!(dynamic_string.data, "Hello, World!");
+    }
+
+    #[test]
+    fn test_dynamic_u8_string_binary_controller() {
+        let string = "Hello, World!".to_string();
+        let controller = DynamicStringBinaryController::<u8>::new();
+        let encoded = controller.encode(string.clone());
+        let data = encoded.get_data();
+        assert_eq!(
+            data,
+            &[13, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33]
+        );
+        let mut binary = PointeredBinary::new(encoded.get_data().clone());
+
+        let decoded = controller.decode(&mut binary);
+        assert_eq!(decoded, string);
+    }
+
+    #[test]
+    fn test_dynamic_u16_string_binary_controller() {
+        let string = "Hello, World!".to_string();
+        let controller = DynamicStringBinaryController::<u16>::new();
+        let encoded = controller.encode(string.clone());
+        let data: &Vec<u8> = encoded.get_data();
+        assert_eq!(
+            data,
+            &[13, 0, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33]
+        );
+        let mut binary = PointeredBinary::new(encoded.get_data().clone());
+
+        let decoded = controller.decode(&mut binary);
+        assert_eq!(decoded, string);
+    }
+    
+    #[test]
+    fn test_sized_string_encodable() {
+        let string = "Hello, World!".to_string();
+        let controller = SizedStringBinaryController::new(20);
+        let encoded = controller.encode(string.clone());
+        let data = encoded.get_data();
+        assert_eq!(
+            data,
+            &[
+                72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33, 0, 0, 0, 0, 0, 0, 0
+            ]
+        );
+    }
+    
+    #[test]
+    fn test_sized_string_decodable() {
+        let data = vec![
+            72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33, 0, 0, 0, 0, 0, 0, 0,
+        ];
+        let mut binary = PointeredBinary::new(data);
+        let controller = SizedStringBinaryController::new(20);
+        let decoded = controller.decode(&mut binary);
+        assert_eq!(decoded, "Hello, World!");
     }
 }
